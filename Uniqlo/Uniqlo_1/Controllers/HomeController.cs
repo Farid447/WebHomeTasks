391d15f36a86ad2d7e224bc980446455f48e2f05
Farid477 <faridrn-af106@code.edu.az>
2024-11-21 05:33:17 +0400

    Task1

---
+++ Uniqlo/Uniqlo_1/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Uniqlo_1.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult About()
+    {
+        return View();
+    }
+    public IActionResult Contact()
+    {
+        return View();
+    }
+}
