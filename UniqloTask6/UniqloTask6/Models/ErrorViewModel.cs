9142e1fddfffa68062a5d0a5c25d66d2625b8f87
Farid477 <faridrn-af106@code.edu.az>
2024-11-29 06:21:43 +0400

    Task6

---
+++ UniqloTask6/UniqloTask6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UniqloTask6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
